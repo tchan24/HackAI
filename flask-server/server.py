9c6a0733135bd3058af28b2d1e1e1a3926a32872
Bfaraira393 <farairab@gmail.com>
2023-04-15 21:10:14 -0500

    push

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,33 +1,11 @@
-import boto3
-import fastparquet
-import pyarrow
-import flask
-import io
-import pandas as pd
-from IPython.display import display
+from flask import Flask
 
-# Set Buffer
-buffer_pbp = io.BytesIO()
-buffer_players = io.BytesIO()
+app = Flask(__name__)
 
-# Create connection to S3
-s3 = boto3.resource('s3', aws_access_key_id = 'AKIAWNNDBSXELJDB2NPI', aws_secret_access_key = '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')
+#Members API Routes
+@app.route('/members')
+def members():
+    return {"members": ["Member1"]}
 
-# Read PBP Data from S3
-pbp_object = s3.Object('utd-hackathon', 'event_pbp.parquet')
-pbp_object.download_fileobj(buffer_pbp)
-
-df_pbp = pd.read_parquet(buffer_pbp)
-
-for col in df_pbp.columns:
-    print(col)
-display(df_pbp)
-
-# Read Players Data from S3
-players_object = s3.Object('utd-hackathon', 'game_players.parquet')
-players_object.download_fileobj(buffer_players)
-
-df_players = pd.read_parquet(buffer_players)
-for col in df_players.columns:
-    print(col)
-display(df_players)
+if __name__ == '__main__':
+    app.run(debug=True)
